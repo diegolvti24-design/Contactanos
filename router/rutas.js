115016b0aa1a467e32821fb18ac9cf96ffa7fe58
diegolvti24-design <diegolv.ti24@utsjr.edu.mx>
2025-09-28 19:34:37 -0600

    Add files via upload

---
+++ router/rutas.js
@@ -0,0 +1,14 @@
+import {Router} from "express"
+const router=Router();
+
+router.get("/", function(req, res){
+	res.render("home")
+})
+router.get("/info", function(req, res){
+	res.render("informacion")
+})
+router.get("/contactanos", function(req, res){
+	res.render("contactanos")
+})
+
+export default router
